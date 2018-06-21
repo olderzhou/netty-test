122a3c8dff315b647ab349661650542d33ee8eaf
klaus <klauszhou@wezhuiyi.com>
2018-06-21 17:42:24 +0800

    [feature] update run script

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@
 echo "$1"
 
 mvn clean package -P "$1" -DskipTests
-mkdir /c/Users/zykj/Data/deploy/"$1" -p
-cp test-eureka/target/netty-test.jar /c/Users/zykj/Data/deploy/"$1"
-cd /c/Users/zykj/Data/deploy/"$1"
+mkdir ~/Data/deploy/"$1" -p
+cp test-eureka/target/netty-test.jar ~/Data/deploy/"$1"
+cd ~/Data/deploy/"$1"
 
 java -jar netty-test.jar
 
